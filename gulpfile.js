d755a301b5bf3c354d6ee98a64d4b36b5e1c7a7e
David Collette <collettedj@gmail.com>
2016-01-21 07:06:01 -0800

    add automated testing on code changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@
 
 const jshint = require('gulp-jshint');
 const gulp   = require('gulp');
+const mocha = require('gulp-mocha');
 const stylish = require('jshint-stylish');
 
 const jshintFiles = ['*.js', 'routes/**/*.js', 'api-routes/**/*.js', 'tests/**/*.js', 'passport/**/*.js'];
+const testFiles = ['tests/**/*test.js'];
  
 gulp.task('lint', function() {
   return gulp.src(jshintFiles)
@@ -12,6 +14,13 @@ gulp.task('lint', function() {
     .pipe(jshint.reporter(stylish));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(jshintFiles, ["lint"]);
+
+gulp.task('test', ['lint'], () => {
+    return gulp.src(testFiles, {read: false})
+        // gulp-mocha needs filepaths so you can't have any plugins before it
+        .pipe(mocha({}));
+});
+
+gulp.task('watch', ['lint', 'test'], function() {
+    gulp.watch(jshintFiles, ['test']);
 }); 
